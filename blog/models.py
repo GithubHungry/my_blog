fb4e2af5a9198bdf972d5593df17d8c8d1f8105b
Vadim <bardiervadim97@gmail.com>
2020-06-22 19:54:34 +0300

    'add_list_and_detail_view'

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    """New manager for filtering published posts."""
+
+    def get_queryset(self):
+        """Method will be executed every Post.published.all() [example]"""
+        return super().get_queryset().filter(status='published')
+
 
 class Post(models.Model):
+    """Model for posts."""
     STATUS_CHOICES = (('draft', 'Draft'), ('published', 'Published'),)
     title = models.CharField(max_length=225)
     slug = models.SlugField(max_length=225, unique_for_date='publish')
@@ -16,10 +25,16 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
+    objects = models.Manager()  # Save objects manager by default
+    published = PublishedManager()  # Add custom manager
+
     class Meta:
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
         ordering = ('-publish',)
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.slug, self.publish.year, self.publish.month, self.publish.day])
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
+from .models import Post
+
 
 # Create your views here.
+# def post_list(request):
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 1)
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    paginate_by = 1
+    template_name = 'blog/post/list.html'
+    context_object_name = 'posts'
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status='published', publish__year=year, publish__month=month, publish__day=day,
+                             slug=post)
+    return render(request, 'blog/post/detail.html', {'object': post})
