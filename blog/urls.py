422f657c369c52fd32049b4dab9b2ad2df7c31c9
GithubHungry <bardiervadim97@gmail.com>
2020-08-16 13:19:48 +0300

    add postgresql search and myvenv_linux

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     # path('', views.PostListView.as_view(), name='post_list'),
     path('detail/<slug:post>/<int:year>/<int:month>/<int:day>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('search/', views.post_search, name='post_search'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
